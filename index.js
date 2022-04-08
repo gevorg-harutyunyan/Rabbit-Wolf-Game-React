a9069bf70ddb0a6a0f850b92281cd18a7d2b830c
Gevorg Harutyunyan <55658181+gevorg-harutyunyan@users.noreply.github.com>
2022-04-09 00:47:20 +0400

    Multiplay

--- index.js
+++ index.js
@@ -22,6 +22,32 @@ const CHARACTERS = {
     }
 }
 
+const isEqual = (a) => (b) => a === b
+const calculateDistance = (a) => (b) => Math.sqrt((b[X] - a[X]) ** 2 + (b[Y] - a[Y]) ** 2)
+const idGenerator = () => Math.floor(Math.random() * 100000)
+
+const renderMatrix = (container, matrix) => {
+    container.innerHTML = ""
+    const table = document.createElement('table')
+    matrix.forEach(row => {
+        const tr = document.createElement('tr')
+        row.forEach(cell => {
+            const td = document.createElement('td')
+            if (cell !== FREE_SPACE) {
+                const img = document.createElement('img')
+                img.src = CHARACTERS[cell].url
+                td.appendChild(img)
+            }
+            tr.appendChild(td)
+        })
+        table.appendChild(tr)
+    })
+    container.appendChild(table)
+}
+
+const renderWinOrLose = (container, winOrLose) => {
+    container.innerHTML = `<h1 class=${winOrLose}>You ${winOrLose}</h1>`
+}
 
 const compose = (...funcs) => {
     if (funcs.length === 0) {
@@ -34,8 +60,6 @@ const compose = (...funcs) => {
     const withoutLastFn = funcs.slice(0, funcs.length - 1);
     return (...args) => compose(...withoutLastFn)(lastFn(...args));
 }
-
-
 
 const createConfig = _mapSize => {
     const MAP_SIZE = _mapSize * 1
@@ -51,14 +75,15 @@ const createConfig = _mapSize => {
     }
 }
 
+const createGame = (container) => {
+    let matrix
+    let config = createConfig(5)
 
-
-const createGame = (_containerID, _mapSizeID) => {
-    const container = document.getElementById(_containerID)
-    let matrix = []
-    let config = createConfig(document.getElementById(_mapSizeID).value)
+    const isInRange = (start, end) => (number) => number >= start && number <= end
+    let _isInRange = isInRange(0, config.MAP_SIZE - 1)
 
     const fillMatrix = () => {
+        matrix = []
         for (let x = 0; x < config.MAP_SIZE; x++) {
             matrix[x] = []
             for (let y = 0; y < config.MAP_SIZE; y++) {
@@ -76,9 +101,11 @@ const createGame = (_containerID, _mapSizeID) => {
         return getFreeRandomPosition()
     }
 
-    const setCharacter = (_character, _position) => {
-        matrix[_position[X]][_position[Y]] = _character
+    const setCharacter = (character, [X, Y]) => {
+        matrix[X][Y] = character
     }
+
+    const removeCharacter = (position) => setCharacter(FREE_SPACE, position)
 
     const setCharacters = (character, count = 1) => {
         for (let i = 0; i < count; i++) {
@@ -93,113 +120,93 @@ const createGame = (_containerID, _mapSizeID) => {
         setCharacters(WALL, config.WALL_COUNT)
     }
 
-    const render = () => {
-        container.innerHTML = ""
-        const table = document.createElement('table')
-        for (let x = 0; x < config.MAP_SIZE; x++) {
-            const tr = document.createElement('tr')
-            for (let y = 0; y < config.MAP_SIZE; y++) {
-                const td = document.createElement('td')
-                if (matrix[x][y] !== FREE_SPACE) {
-                    const img = document.createElement('img')
-                    img.src = CHARACTERS[matrix[x][y]].url
-                    td.appendChild(img)
-                }
-                tr.appendChild(td)
-            }
-            table.appendChild(tr)
-        }
-        container.appendChild(table)
-    }
-
     const getCharacterPositions = character => {
-        let result = []
-        for (let x = 0; x < config.MAP_SIZE; x++) {
-            for (let y = 0; y < config.MAP_SIZE; y++) {
-                if (matrix[x][y] === character) {
-                    result.push([x, y])
+        return matrix.reduce((positions, row, X) => {
+            return [...positions, ...row.reduce((rowPositions, cell, Y) => {
+                if (cell === character) {
+                    return [...rowPositions, [X, Y]]
                 }
-            }
-        }
-        return result
+                return rowPositions
+            }, [])]
+        }, [])
     }
 
-    const checkTeleport = (_position, mapSize) => {
-        if (_position[X] === mapSize) {
-            _position[X] = 0
-        } else if (_position[X] === -1) {
-            _position[X] = mapSize - 1
+    const checkTeleport = ([X, Y], mapSize) => {
+        const isXEqual = isEqual(X)
+        const isYEqual = isEqual(Y)
+        if (isXEqual(mapSize)) {
+            X = 0
+        } else if (isXEqual(-1)) {
+            X = mapSize - 1
         }
-        if (_position[Y] === mapSize) {
-            _position[Y] = 0
-        } else if (_position[Y] === -1) {
-            _position[Y] = mapSize - 1
+        if (isYEqual(mapSize)) {
+            Y = 0
+        } else if (isYEqual(-1)) {
+            Y = mapSize - 1
         }
-        return [_position[X], _position[Y]]
+        return [X, Y]
     }
 
-    const isAvailableField = (character, _position) => {
-        const availableFields = CHARACTERS[character].availableFields
-        for (let i = 0; i < availableFields.length; i++) {
-            if (matrix[_position[X]][_position[Y]] === availableFields[i]) {
-                return true
-            }
-        }
-        return false
+    const isAvailableField = (character, [X, Y]) => {
+        return CHARACTERS[character]
+            .availableFields
+            .some(isEqual(matrix[X][Y]))
     }
 
-    const isWinOrLose = (_position1, _position2) => {
-        const position1 = matrix[_position1[X]][_position1[Y]]
-        const position2 = matrix[_position2[X]][_position2[Y]]
-        if (position1 === RABBIT && position2 === HOME) {
-            config.isGameInProcess = false
-            container.innerHTML = '<h1 class="win">You Win</h1>'
+    const chackWinOrLose = ([X, Y], [newX, newY]) => {
+        const isCurrentPositionEqual = isEqual(matrix[X][Y])
+        const isNewPositionEqual = isEqual(matrix[newX][newY])
+
+        if (isCurrentPositionEqual(RABBIT) && isNewPositionEqual(HOME)) {
+            return 'Win'
         } else if (
-            (position1 === RABBIT && position2 === WOLF)
-            ||
-            (position1 === WOLF && position2 === RABBIT)) {
+            isCurrentPositionEqual(RABBIT) && isNewPositionEqual(WOLF) ||
+            isCurrentPositionEqual(WOLF) && isNewPositionEqual(RABBIT)) {
+            return 'Lose'
+        }
+    }
+
+    const characterStep = ([currentX, currentY], [nextX, nextY]) => {
+        const winOrLose = chackWinOrLose([currentX, currentY], [nextX, nextY])
+        if (winOrLose === 'Win' || winOrLose === 'Lose') {
             config.isGameInProcess = false
-            container.innerHTML = '<h1 class="lose">You Lose</h1>'
+            renderWinOrLose(container, winOrLose)
         }
-    }
-
-    const characterStep = (_currentPosition, _nextPosition) => {
-        isWinOrLose(_currentPosition, _nextPosition)
-        matrix[_nextPosition[X]][_nextPosition[Y]] = matrix[_currentPosition[X]][_currentPosition[Y]]
-        matrix[_currentPosition[X]][_currentPosition[Y]] = FREE_SPACE
+        const character = matrix[currentX][currentY]
+        setCharacter(character, [nextX, nextY])
+        removeCharacter([currentX, currentY])
         if (config.isGameInProcess) {
-            render()
+            renderMatrix(container, matrix)
         }
     }
 
-    const neighborPositions = currentPosicion => {
+    const neighborPositions = ([X, Y]) => {
         return [
-            [currentPosicion[X], currentPosicion[Y] + 1],
-            [currentPosicion[X], currentPosicion[Y] - 1],
-            [currentPosicion[X] + 1, currentPosicion[Y]],
-            [currentPosicion[X] - 1, currentPosicion[Y]]
+            [X, Y + 1],
+            [X, Y - 1],
+            [X + 1, Y],
+            [X - 1, Y]
         ]
     }
-    
-    const filterIllegalPositions = neighborPositions => {
-        return neighborPositions.filter(position => position[X] >= 0 && position[X] < config.MAP_SIZE && position[Y] >= 0 && position[Y] < config.MAP_SIZE)
-    }
+
+    const filterIllegalPositions = neighborPositions => neighborPositions.filter(([X, Y]) => _isInRange(X) && _isInRange(Y))
 
     const filterUnavailableFields = positions => positions.filter(position => isAvailableField(WOLF, position))
 
     const getDistancesFromRabbit = positions => {
         const rabbit = getCharacterPositions(RABBIT)[0]
-        const distances = positions.map(position => Math.sqrt((position[X] - rabbit[X]) ** 2 + (position[Y] - rabbit[Y]) ** 2))
+        const calculateDistanceFromRabbit = calculateDistance(rabbit)
+        const distances = positions.map(position => calculateDistanceFromRabbit(position))
         return {
             distances,
             positions
         }
     }
 
-    const getOptimalPosition = ({distances, positions}) =>  positions[distances.indexOf(Math.min(...distances))]
+    const getShortestDistancePosition = ({ distances, positions }) => positions[distances.indexOf(Math.min(...distances))]
 
     const wolfNextPosition = compose(
-        getOptimalPosition,
+        getShortestDistancePosition,
         getDistancesFromRabbit,
         filterUnavailableFields,
         filterIllegalPositions,
@@ -210,20 +217,23 @@ const createGame = (_containerID, _mapSizeID) => {
         getCharacterPositions(WOLF)
             .forEach(wolf => {
                 if (config.isGameInProcess) {
-                    characterStep(wolf, wolfNextPosition(wolf))
+                    const nextPosition = wolfNextPosition(wolf)
+                    if (nextPosition !== undefined) {
+                        characterStep(wolf, nextPosition)
+                    }
                 }
             })
     }
 
-    const rabbitStep = _coordinates => {
-        const currentPosition = getCharacterPositions(RABBIT)[0]
-        const nextPosition = checkTeleport([currentPosition[X] + _coordinates[X], currentPosition[Y] + _coordinates[Y]], config.MAP_SIZE)
+    const rabbitStep = ([x, y]) => {
+        const rabbit = getCharacterPositions(RABBIT)[0]
+        const nextPosition = checkTeleport([rabbit[X] + x, rabbit[Y] + y], config.MAP_SIZE)
 
         if (!config.isGameInProcess) {
             return
         }
         if (isAvailableField(RABBIT, nextPosition)) {
-            characterStep(currentPosition, nextPosition)
+            characterStep(rabbit, nextPosition)
             wolvesStep()
         }
     }
@@ -246,12 +256,13 @@ const createGame = (_containerID, _mapSizeID) => {
         }
     }
 
-    const start = (_mapSizeID) => {
-        config = createConfig(document.getElementById(_mapSizeID).value)
+    const start = (mapSize) => {
+        config = createConfig(mapSize)
+        _isInRange = isInRange(0, config.MAP_SIZE - 1)
         config.isGameInProcess = true
         fillMatrix()
         fillMatrixWithCharacters()
-        render()
+        renderMatrix(container, matrix)
     }
 
     return {
@@ -260,12 +271,48 @@ const createGame = (_containerID, _mapSizeID) => {
     }
 }
 
-const game = createGame('container', 'mapSize')
 
-const play = () => {
-    game.start('mapSize')
+
+const newGame = () => {
+    const container = document.getElementById('container')
+    const id = idGenerator()
+    
+    const gameBlock = document.createElement('div')
+
+    gameBlock.setAttribute("id", id)
+    gameBlock.setAttribute("class", "game")
+
+    gameBlock.innerHTML = `
+    <select id="mapSize-${id}">
+        <option value="5">5X5</option>
+        <option value="7">7X7</option>
+        <option value="9">9X9</option>
+    </select>
+    <button id="playGameButton-${id}">Play Game</button>
+    <button id="deleteGameButton-${id}" class="delete">&#x2716;</button>
+
+    <div id="gameContainer-${id}" class="gameContainer"></div>
+
+    <div><button id="ArrowUp-${id}">&#x21e1;</button></div>
+    <button id="ArrowLeft-${id}">&#x21e0;</button>
+    <button id="ArrowDown-${id}">&#x21e3;</button>
+    <button id="ArrowRight-${id}">&#x21e2;</button>
+    `
+
+    container.appendChild(gameBlock)
+    
+    document.getElementById("playGameButton-" + id)
+    .onclick = () => game.start(document.getElementById("mapSize-" + id).value)
+
+    document.getElementById("deleteGameButton-" + id)
+    .onclick = () => gameBlock.remove()
+    
+    const gameContainer = document.getElementById("gameContainer-" + id)
+
+    document.getElementById("ArrowLeft-" + id).onclick = () => game.event("ArrowLeft")
+    document.getElementById("ArrowUp-" + id).onclick = () => game.event("ArrowUp")
+    document.getElementById("ArrowDown-" + id).onclick = () => game.event("ArrowDown")
+    document.getElementById("ArrowRight-" + id).onclick = () => game.event("ArrowRight")
+    
+    const game = createGame(gameContainer)
 }
-
-window.addEventListener('keyup', evn => {
-        game.event(evn.key)
-    })
